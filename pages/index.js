544b10e9ae93268dbc9d991ec8245106aa0f9710
kento <kento@toriwatari.bird>
2021-10-16 20:49:24 +0900

    🏃‍♂️ Update index.tsx

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import { gql, GraphQLClient } from "graphql-request"
-import { Component } from "react";
 
 export const getStaticProps = async () => {
   const url = "https://api-ap-northeast-1.graphcms.com/v2/cktqjrx81222301xm7c7c0rpw/master"
   const graphQLClient = new GraphQLClient(url, {
     headers: {
-      "Authorization": "Bearer ここにアクセストークン入れる"
+      "Authorization": `Bearer ${process.env.API_KEY}`
     }
   });
 
@@ -26,10 +25,18 @@ export const getStaticProps = async () => {
   }
 }
 
-const Home = (data) => {
-  console.log(data)
+const Home = ({data}) => {
+  const { authors } = data
+
   return (
-    <div>Heloo</div>
+    <div>
+    {authors.map((author) => (
+      <p key={author.name}>{author.name}</p>
+    ))
+}
+
+    </div>
+
   )
 }
 
