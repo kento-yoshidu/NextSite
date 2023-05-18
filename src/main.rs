0f31253558f1a678798bd3890d0dd3466cba337d
kento <kento@toriwatari.bard>
2023-05-18 19:51:45 +0900

    ✨ Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{middleware, get, App, HttpServer, Responder};
+use dotenv::dotenv;
+use listenfd::ListenFd;
+use serde::__private::ser;
+use std::env;
 
 #[get("/hello")]
 async fn hello_world() -> impl Responder {
@@ -7,11 +11,26 @@ async fn hello_world() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+
+    let mut listenfd = ListenFd::from_env();
+
+    let mut server = HttpServer::new(|| {
         App::new()
             .service(hello_world)
-    })
-    .bind("127.0.0.1:4000")?
-    .run()
-    .await
+            .wrap(middleware::Logger::default())
+    });
+
+    env_logger::init();
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = env::var("HOST").expect("Please set host in .env");
+            let port = env::var("PORT").expect("Please set port in .env");
+            server.bind(format!("{}:{}", host, port))?
+        }
+    };
+
+    server.run().await
 }
